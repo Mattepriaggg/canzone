7502b768da763b4d2c7b2bb0719887c45218bfd9
Mattepriagggg <mattepriaggg@gmail.com>
2023-03-08 19:00:18 +0100

    1

---
+++ index.js
@@ -0,0 +1,20 @@
+function controllaParolakk08765hh1() {
+    var parola = document.getElementById("kk08765hh1").value;
+    if (parola.toLowerCase() == "ciao") {
+        window.location.href = "pass-2.html";
+    } else {
+        alert("Riprova");
+    }
+}
+
+function controllaParolakk08765hh2() {
+    var parola = document.getElementById("kk08765hh2").value;
+    if (parola.toLowerCase() == "ciao") {
+        window.location.href = "pass-3.html";
+    } else {
+        alert("Riprova dall' inizio!");
+        window.location.href = "pass-1.html";
+    }
+}
+
+
